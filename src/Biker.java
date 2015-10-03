a77e1364d23b475cb84661bceed2e5bd163a81a0
mKals <m.kals@me.com>
2015-10-02 21:42:58 -0700

    Cahnges to isAlive in Biker class (getter updated, setter updated and updated local variable name)

--- src/Biker.java
+++ src/Biker.java
@@ -7,7 +7,7 @@ public class Biker {
     private BikerAI ai;
     
     //True if the biker is still alive
-    private boolean status;
+    private boolean isAlive;
     
     //TODO:Implement
     public Biker(int Xpos,int Ypos, BikerAI ai){
@@ -19,7 +19,18 @@ public class Biker {
         return BikerActions.FORWARDS;
     }
     
-    public boolean isDead(){
-        return !status;
+    /**
+     * sets the status of the Biker to dead
+     */
+    public void murder() {
+        isAlive = false;
+    }
+    
+    /**
+     * getter for life status of Biker
+     * @return true if biker is alive.
+     */
+    public boolean isAlive(){
+        return isAlive;
     }
 }
