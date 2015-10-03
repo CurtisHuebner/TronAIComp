3c2a27c3188cd44b4c0c7f6271e0bcf581314769
Curtis Huebner <mathcurt@gmail.com>
2015-10-02 20:21:45 -0700

    Added some stuff

--- src/TronGame.java
+++ src/TronGame.java
@@ -1,4 +1,21 @@
 
 public class TronGame {
-
+    private BoardItem[][] board;
+    private Biker[] bikers;
+    
+    //TODO: implement
+    public TronGame(int boardSize, BikerAI[] players){
+        
+    }
+    
+    //TODO: implement
+    public void tick(){
+        
+    }
+    
+    public enum direction{
+        NORTH,SOUTH,EAST,WEST
+    }
+    
+    
 }
